484a5a713ff873db1bb65fe640ec60f234772c48
eycwave <emreyuce000@gmail.com>
2023-10-05 20:48:28 +0300

    student class created

---
+++ src/Student.java
@@ -0,0 +1,41 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Student {
+    String studentName;
+    String studentNumber;
+    String studentEmail;
+    Set<String> enrolledCourses = new HashSet<>();
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public Set<String> getEnrolledCourses() {
+        return enrolledCourses;
+    }
+
+    public void setEnrolledCourses(Set<String> enrolledCourses) {
+        this.enrolledCourses = enrolledCourses;
+    }
+}
