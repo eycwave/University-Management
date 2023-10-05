6ddb81a9637ecf83525662d9f2cf62f07ebb75cc
eycwave <emreyuce000@gmail.com>
2023-10-05 20:56:15 +0300

    instructor class created

---
+++ src/Instructor.java
@@ -0,0 +1,41 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Instructor {
+    String instructorName;
+    String instructorNumber;
+    String instructorEmail;
+    Set<String> taughtCourses = new HashSet<>();
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        this.instructorName = instructorName;
+    }
+
+    public String getInstructorNumber() {
+        return instructorNumber;
+    }
+
+    public void setInstructorNumber(String instructorNumber) {
+        this.instructorNumber = instructorNumber;
+    }
+
+    public String getInstructorEmail() {
+        return instructorEmail;
+    }
+
+    public void setInstructorEmail(String instructorEmail) {
+        this.instructorEmail = instructorEmail;
+    }
+
+    public Set<String> getTaughtCourses() {
+        return taughtCourses;
+    }
+
+    public void setTaughtCourses(Set<String> taughtCourses) {
+        this.taughtCourses = taughtCourses;
+    }
+}
