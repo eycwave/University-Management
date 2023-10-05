92fad4d8a67b9ea96b385bc1d25e4308856a4823
eycwave <emreyuce000@gmail.com>
2023-10-05 20:07:22 +0300

    student class created.

---
+++ src/Course.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Course {
+    String courseName;
+    String courseNumber;
+
+
+
+
+}
---
+++ src/ServiceInstructor.java
@@ -0,0 +1,13 @@
+public class ServiceInstructor {
+
+    private Instructor instructor;
+
+    public ServiceInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public void teach(){
+        // öğretmenin kurs dersi verme işlemi gerçekleştirilir.
+    }
+
+}
