07b56f71dcd85f79aa7ea26d22e0f254d2d7ba66
eycwave <emreyuce000@gmail.com>
2023-10-09 17:10:48 +0300

    made changes after review

--- src/Course.java
+++ src/Course.java
@@ -1,18 +1,26 @@
 public class Course {
 
-    String name;
-    int number;
+    private String name;
+    private int number;
 
-    public Course(String name, int number) {
-        this.name = name;
-        this.number = number;
+    public Course() {
+        this.name = "Bilinmiyor";
+        this.number = 0;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getNumber() {
         return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
     }
 }
